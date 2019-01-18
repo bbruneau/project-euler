462b94f118df46cc9da9b374e1c2e081138043db
Benjamin Bruneau <benbruneau@gmail.com>
2019-01-17 23:34:22 -0500

    improve my first answer

--- 001/run.js
+++ 001/run.js
@@ -2,9 +2,16 @@ const R = require("ramda");
 
 const max = process.argv.slice(2)[0] || 1000;
 
+const checkIntAndSum = val => currentSum =>
+  val % 3 === 0 || val % 5 === 0 ? currentSum + val : currentSum;
+
+const getNextValArr = ([currentNum, sum]) => [
+  currentNum + 1,
+  checkIntAndSum(currentNum)(sum)
+];
+
 console.log(
-  (max =>
-    R.mapAccum(
-      (acc, val) => (val % 3 === 0 || val % 5 === 0 ? [acc + val] : [acc])
-    )(0)(R.times(R.identity)(max))[0])(max)
+  R.takeLast(1)(
+    R.until(([count]) => R.gte(count)(max))(getNextValArr)([0, 0])
+  )[0]
 );
