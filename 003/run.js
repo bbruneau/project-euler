ff85dd2ba6c24b0a73e395b9622601159bf85994
Benjamin Bruneau <benbruneau@gmail.com>
2019-01-18 14:16:38 -0500

    non-functional solution to 3

---
+++ 003/run.js
@@ -0,0 +1,45 @@
+const R = require("ramda");
+
+const max = parseInt(process.argv.slice(2)[0]) || 600851475143;
+
+const appendNextPrime = (primes = [2]) => {
+  const primesCount = primes.length;
+  let cv = primes[primesCount - 1] + 1;
+
+  while (primes.length === primesCount) {
+    if (!primes.some(prime => cv % prime === 0)) {
+      primes.push(cv);
+    }
+
+    cv++;
+  }
+  return primes;
+};
+
+// initialValues: [
+//   testedPrimes: Array<number>,
+//   highestPrimeFactor?: number,
+//   lastProduct: number
+// ]
+const initialValues = [[2], null, max];
+
+const checkForPrimeFactor = ([primes, highestPrimeFactor, lastProduct]) => {
+  const prime = R.takeLast(1)(primes)[0];
+
+  if (lastProduct % prime === 0) {
+    lastProduct = lastProduct / prime;
+    highestPrimeFactor = prime;
+  }
+
+  primes = appendNextPrime(primes);
+
+  return [primes, highestPrimeFactor, lastProduct];
+};
+
+console.log(
+  R.takeLast(1)(
+    R.until(([primes, _, lastProduct]) =>
+      R.gte(R.takeLast(1)(primes))(lastProduct)
+    )(checkForPrimeFactor)(initialValues)
+  )[0]
+);
