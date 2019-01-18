42d40949a717e611dac3e9240a8c69e399adec30
Benjamin Bruneau <benbruneau@gmail.com>
2019-01-18 17:14:08 -0500

    replace Array.some with R.none

--- 003/run.js
+++ 003/run.js
@@ -2,25 +2,21 @@ const R = require("ramda");
 
 const max = parseInt(process.argv.slice(2)[0]) || 600851475143;
 
-// const sieveForPrime = (primes = [2]) => {
-//   const primesCount = primes.length;
-//   let cv = primes[primesCount - 1] + 1;
-
-//   while (primes.length === primesCount) {
-//     if (!primes.some(prime => cv % prime === 0)) {
-//       primes.push(cv);
-//     }
-
-//     cv++;
-//   }
-//   return primes;
-// };
+// const sieveForPrime = (prevPrimes = [2]) =>
+//       R.head(
+//         R.until(([primes]) => R.gt(primes.length)(prevPrimes.length))(
+//           ([primes, possiblePrime]) =>
+//             !primes.some(prime => possiblePrime % prime === 0)
+//             ? [[...primes, possiblePrime], null]
+//             : [primes, possiblePrime + 1]
+//         )([prevPrimes, R.last(prevPrimes) + 1])
+//       );
 
 const sieveForPrime = (prevPrimes = [2]) =>
   R.head(
     R.until(([primes]) => R.gt(primes.length)(prevPrimes.length))(
       ([primes, possiblePrime]) =>
-        !primes.some(prime => possiblePrime % prime === 0)
+        R.none(prime => possiblePrime % prime === 0)(primes)
           ? [[...primes, possiblePrime], null]
           : [primes, possiblePrime + 1]
     )([prevPrimes, R.last(prevPrimes) + 1])
