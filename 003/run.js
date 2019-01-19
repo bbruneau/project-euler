2742aa9fa2160c4e3536e2953f48e9bd19db92a3
Benjamin Bruneau <benbruneau@gmail.com>
2019-01-18 19:57:35 -0500

    make discrete funcs for each step so it's easier to read

--- 003/run.js
+++ 003/run.js
@@ -9,34 +9,51 @@ const max = parseInt(process.argv.slice(2)[0]) || 600851475143;
 // ]
 const initialValues = [[2], max, null];
 
+const numberHasNoPrimeFactors = ([primes, possiblePrime]) =>
+  R.none(prime => R.equals(R.modulo(possiblePrime)(prime))(0));
+const addPrimeToPrimesList = ([primes, possiblePrime]) => [
+  R.flatten([primes, possiblePrime]),
+  null
+];
+const incrementPossiblePrime = ([primes, possiblePrime]) => [
+  primes,
+  R.add(possiblePrime)(1)
+];
+
 const sieveForPrime = (prevPrimes = [2]) =>
   R.head(
     R.until(([primes]) => R.gt(primes.length)(prevPrimes.length))(
-      ([primes, possiblePrime]) =>
-        R.ifElse(R.none(prime => R.equals(R.modulo(possiblePrime)(prime))(0)))(
-          () => [R.flatten([primes, possiblePrime]), null]
-        )(() => [primes, R.add(possiblePrime)(1)])(primes)
+      R.ifElse(numberHasNoPrimeFactors)(addPrimeToPrimesList)(
+        incrementPossiblePrime
+      )
     )([prevPrimes, R.add(R.last(prevPrimes))(1)])
   );
 
-const checkForPrimeFactor = ([primes, lastProduct, highestPrimeFactor]) => {
-  return R.ifElse((primes, lastProduct, highestPrimeFactor) =>
-    R.equals(R.modulo(lastProduct)(R.last(primes)))(0)
-  )(() => [
-    sieveForPrime(primes),
-    lastProduct / R.last(primes),
-    R.last(primes)
-  ])(() => [sieveForPrime(primes), lastProduct, highestPrimeFactor])(
+const isPrimeFactor = (primes, lastProduct, highestPrimeFactor) =>
+  R.equals(R.modulo(lastProduct)(R.last(primes)))(0);
+
+const setNewPrimeFactor = (primes, lastProduct) => [
+  sieveForPrime(primes),
+  R.divide(lastProduct)(R.last(primes)),
+  R.last(primes)
+];
+
+const incrementPotentialPrimeFactor = (
+  primes,
+  lastProduct,
+  highestPrimeFactor
+) => [sieveForPrime(primes), lastProduct, highestPrimeFactor];
+
+const getNextPrimeFactor = ([primes, lastProduct, highestPrimeFactor]) =>
+  R.ifElse(isPrimeFactor)(setNewPrimeFactor)(incrementPotentialPrimeFactor)(
     primes,
     lastProduct,
     highestPrimeFactor
   );
-};
+
+const lastPrimeIsGTELastProduct = ([primes, lastProduct]) =>
+  R.gt(R.last(primes))(lastProduct);
 
 console.log(
-  R.last(
-    R.until(([primes, lastProduct]) => R.gt(R.last(primes))(lastProduct))(
-      checkForPrimeFactor
-    )(initialValues)
-  )
+  R.last(R.until(lastPrimeIsGTELastProduct)(getNextPrimeFactor)(initialValues))
 );
