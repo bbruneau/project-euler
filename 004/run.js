8cfb302753e43117eda8ef414d14dbb8a0976b0a
Benjamin Bruneau <benbruneau@gmail.com>
2019-01-25 20:23:12 -0500

    WIP

--- 004/run.js
+++ 004/run.js
@@ -7,60 +7,44 @@ const lowNum = parseInt(R.join("")(new Array(digits - 1).fill(9)));
 const isPalindrome = num =>
   R.equals(R.toString(num))(R.pipe(R.toString, R.reverse)(num));
 
-const hasFoundPalindrome = ([a, lowNum, lastA, palindrome]) =>
+const hasFoundPalindrome = ([a, palindrome, lowNum, lastA]) =>
   R.or(R.not(R.isNil(palindrome)))(R.or(R.lte(a)(lowNum))(R.lte(a)(lastA)));
 
-const hasNotFoundPalindrome = ([a, lowNum, lastA, palindrome]) =>
-  R.and(R.equals(palindrome)(null))(R.all(R.gt(a))([lowNum, lastA]));
-
 const checkForPalindromes = (
-  b = baseNum,
-  maxPalindrome = 0,
-  lastA = lowNum
-) => {
-  let a = baseNum;
-  let palindrome = null;
-
-  const result = R.until(hasFoundPalindrome)(([a, lowNum, lastA]) => {
-    if (isPalindrome(a * b)) {
-      return [a, lowNum, lastA, a * b];
-    } else {
-      return [a - 1, lowNum, lastA, null];
-    }
-  })([a, lowNum, lastA, null]);
-  // while (hasNotFoundPalindrome([a, lowNum, lastA, palindrome])) {
-  //   console.log(
-  //     "hasFoundPalindrome",
-  //     hasFoundPalindrome([a, lowNum, lastA, palindrome])
-  //   );
-  //   if (isPalindrome(a * b)) {
-  //     palindrome = a * b;
-  //   } else {
-  //     a--;
-  //   }
-  // }
-  // return [Math.max(palindrome, maxPalindrome), a, b];
-  return [Math.max(result[3], maxPalindrome), result[0]];
-};
+  lastA = lowNum,
+  factorB = baseNum,
+  maxPalindrome = 0 // => [factorA of resultant palindrome, palindrome | null]
+) =>
+  R.slice(0)(2)(
+    R.until(hasFoundPalindrome)(([factorA, _, lowNum, lastA]) =>
+      R.ifElse(isPalindrome)(() => [
+        factorA,
+        R.max(R.multiply(factorA)(factorB))(maxPalindrome),
+        lowNum,
+        lastA
+      ])(() => [factorA - 1, null, lowNum, lastA])(factorA * factorB)
+    )([baseNum, null, lowNum, lastA])
+    // [factorA, palindrome | null, factor's lower bound, last factor A result]
+  );
 
 let maxPalindrome = 0;
 let biggestA = baseNum;
 let biggestB = baseNum;
-let b = baseNum;
+let factorB = baseNum;
 let lastA = lowNum;
 
-while (b > lowNum && b > lastA) {
-  console.log(b, maxPalindrome, lastA);
-  const [newMax, newA] = checkForPalindromes(b, maxPalindrome, lastA);
+while (R.both(R.lte(lowNum))(R.lte(lastA))(factorB)) {
+  const [newA, newMax] = checkForPalindromes(lastA, factorB, maxPalindrome);
 
   if (newMax !== maxPalindrome && newMax > maxPalindrome) {
+    maxPalindrome = newMax;
     biggestA = newA;
-    biggestB = b;
+    biggestB = factorB;
   } else {
-    b--;
+    factorB--;
   }
 
-  maxPalindrome = newMax;
   lastA = Math.max(lastA, newA);
 }
+
 console.log("MAX", maxPalindrome, biggestA, biggestB);
